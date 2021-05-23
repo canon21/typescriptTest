954a43ce7d39ac520f3f9b423ad8da061e7df6ff
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-05-23 11:55:02 +0200

    Function

--- index.ts
+++ index.ts
@@ -16,7 +16,8 @@ const myNumber: number = 123;
 Typescript assegna il tipo in automatico alla prima assegnazione.(Type Inference)
 */
 //Example
-let myName2 = "Tommy";
+let myName2 = "Tommy"; //assegna alla variabile myName2 il tipo string.
+
 //myName2 = 2; -> Errore di compilazione perchè typescript ha assegnato il tipo string a myName2.
 
 //Alcune eccezioni. Type inference assegna il tipo specifico solo quando il valore e la dichiarazione sono sulla stessa riga.
@@ -27,4 +28,20 @@ let test; //così assegna any.
 test = "tommy";
 test = 2;
 
-//
+// *********************************************** FUNCTION **************************************************************
+//L'assegnazione dei tipi è utile per correggere alcuni errori che si possono commettere durante la scrittura del codice.
+
+//Con assegnazione del tipo.
+const add = (a: number, b: number) => {
+    return a + b; //Type inference here.. sul ritorno
+}
+
+//Con assegnazione del tipo e ritorno annotato
+const add2 = (a: number, b: number): number => {
+    return a + b; //Type inference here.. sul ritorno
+}
+
+
+function joinString (a: string, b: string): string {
+    return a + b;
+}
