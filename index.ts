d5b7d03fc4bae493677d8381ce041c4dc0f196e8
Tommaso Pirozzi <tommasopirozzi@hotmail.it>
2021-05-23 11:36:09 +0200

    Type Inference

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-//BASIC TYPES:
+// ************************************************ BASIC TYPES ****************************************************************
 /*
 string,
 number,
@@ -10,3 +10,21 @@ any -> qualsiasi type
 //Example
 const myName: string = "Tommy";
 const myNumber: number = 123;
+
+// *********************************************** TYPE INFERENCE **************************************************************
+/*
+Typescript assegna il tipo in automatico alla prima assegnazione.(Type Inference)
+*/
+//Example
+let myName2 = "Tommy";
+//myName2 = 2; -> Errore di compilazione perchè typescript ha assegnato il tipo string a myName2.
+
+//Alcune eccezioni. Type inference assegna il tipo specifico solo quando il valore e la dichiarazione sono sulla stessa riga.
+
+//Example.
+let test; //così assegna any.
+
+test = "tommy";
+test = 2;
+
+//
